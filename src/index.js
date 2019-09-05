c2cd30658c3adfaa6535a365e799dd8be233a65b
Shahed Nasser <shahednasser@gmail.com>
2019-09-05 11:44:53 +0000

    fix security vulnerability

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function create() {
     for (let i = 0; i < 9; i++) {
         //add the big square
         const bigSquare = this.add.rectangle(positionX, positionY, bigSquareWidth, bigSquareHeight, 0xffffff).setStrokeStyle(2, 0x000000);
-        gameStatus.squares.push({bigSquare, smallSqaures: []});
+        gameStatus.squares.push({bigSquare, smallSquares: []});
         const initialPositionX = positionX - (bigSquareWidth/2) + (smallSquareWidth/2);
         let smallSquarePositionX = initialPositionX,
             smallSquarePositionY = positionY - (bigSquareHeight/2) + (smallSquareHeight/2);
@@ -51,7 +51,7 @@ function create() {
         for(let k = 0; k < 9; k ++) {
             const square = this.add.rectangle(smallSquarePositionX, smallSquarePositionY, smallSquareWidth,
                                                 smallSquareHeight, 0xFFFFFF).setStrokeStyle(1, 0xd3d3d3);
-            gameStatus.squares[i].smallSqaures.push(square);
+            gameStatus.squares[i].smallSquares.push(square);
             if(k === 2 || k === 5) {
                 smallSquarePositionY += smallSquareHeight;
                 smallSquarePositionX = initialPositionX;
@@ -59,34 +59,33 @@ function create() {
                 smallSquarePositionX += smallSquareWidth;
             }
             //make interactive
-            gameStatus.squares[i].smallSqaures[k].setInteractive({
+            gameStatus.squares[i].smallSquares[k].setInteractive({
                 useHandCursor: true
             });
             //add pointer events
-            gameStatus.squares[i].smallSqaures[k].on('pointerover', function(pointer, localX, localY, event) {
+            gameStatus.squares[i].smallSquares[k].on('pointerover', function(pointer, localX, localY, event) {
                 if(gameStatus.clickedSquare.bigSquare === -1 && gameStatus.clickedSquare.smallSquare === -1){
-                    console.log("here");
-                    square.setFillStyle(0xF0F8FF);
+                    this.square.setFillStyle(0xF0F8FF);
                 }
-            }, {square: gameStatus.squares[i].smallSqaures[k]});
+            }, {square: gameStatus.squares[i].smallSquares[k]});
 
-            gameStatus.squares[i].smallSqaures[k].on('pointerout', function(pointer, localX, localY, event){
-                if(gameStatus.clickedSquare.bigSquare === -1 && gameStatus.clickedSquare.smallSqaure === -1){
-                    square.setFillStyle(0xFFFFFF);
+            gameStatus.squares[i].smallSquares[k].on('pointerout', function(pointer, localX, localY, event){
+                if(gameStatus.clickedSquare.bigSquare === -1 && gameStatus.clickedSquare.smallSquare === -1){
+                    this.square.setFillStyle(0xFFFFFF);
                 }
-            }, {square: gameStatus.squares[i].smallSqaures[k]});
+            }, {square: gameStatus.squares[i].smallSquares[k]});
 
-            gameStatus.squares[i].smallSqaures[k].on('pointerup', function(pointer, localX, localY, event){
+            gameStatus.squares[i].smallSquares[k].on('pointerup', function(pointer, localX, localY, event){
                 if(gameStatus.clickedSquare.bigSquare !== -1 && gameStatus.clickedSquare.smallSquare !== -1) {
                     gameStatus.squares[gameStatus.clickedSquare.bigSquare].smallSquares[gameStatus.clickedSquare.smallSquare].setFillStyle(0xFFFFFF);
                     gameStatus.clickedSquare.bigSquare = -1;
                     gameStatus.clickedSquare.smallSquare = -1;
 
                 } else {
-                    gameStatus.clickedSquare.bigSquare = bigSquare;
-                    gameStatus.clickedSquare.smallSquare = smallSquare;
+                    gameStatus.clickedSquare.bigSquare = this.bigSquare;
+                    gameStatus.clickedSquare.smallSquare = this.smallSquare;
                 }
-            }, {square: gameStatus.squares[i].smallSqaures[k], bigSquare: i, smallSquare: k});
+            }, {square: gameStatus.squares[i].smallSquares[k], bigSquare: i, smallSquare: k});
         }
         if(i === 2 || i === 5) {
             positionY += bigSquareHeight;
